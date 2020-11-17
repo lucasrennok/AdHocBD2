e99d1fb2ed0f8fa9a8165428184617f03e02234f
Edgar Almeida <edgardc.almeida@outlook.com>
2020-11-16 22:23:04 -0300

    autocomplete

--- web/src/components/Selection.tsx
+++ web/src/components/Selection.tsx
@@ -1,19 +1,31 @@
-import {FormControl, FormHelperText, MenuItem, Select} from '@material-ui/core'
+import {FormControl, FormHelperText, MenuItem, Select, TextField} from '@material-ui/core'
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import React from 'react';
 
 interface Props {
-    ligas: string[];
+    typeData: string,
+    data: string[];
 }
 
 export default function Selection(props: Props){
-    const [liga, setLiga] = React.useState('');
+    const [valueSelection, setValue] = React.useState('');
     const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setLiga(event.target.value as string);
+        setValue(event.target.value as string);
     };
+    const allThings = props.data[0]
 
     return(
         <FormControl>
-            <Select
+            <Autocomplete
+                id="combo-box"
+                options={props.data}
+                getOptionLabel={(option) => option}
+                style={{ width: 300 }}
+                defaultValue= {allThings}
+                renderInput={(params) => <TextField {...params} label={props.typeData} variant="outlined" />}
+            />
+
+            {/* <Select
                 value={liga}
                 onChange={handleChange}
                 displayEmpty
@@ -23,7 +35,7 @@ export default function Selection(props: Props){
                 })}
                 
             </Select>
-            <FormHelperText>Selecione uma ou todas as ligas para a consulta</FormHelperText>
+            <FormHelperText>Selecione uma ou todas as ligas para a consulta</FormHelperText> */}
         </FormControl>
     )
 }
